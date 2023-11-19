82b317bcd524d7f2463bd4a84e7e9cf88f4c56f4
Pavel <palllkaignatev@gmail.com>
2023-11-20 01:01:05 +0400

    first commit

--- chat_parser.py
+++ chat_parser.py
@@ -4,48 +4,124 @@ import asyncio
 import json
 from telethon import TelegramClient
 import datetime
-from telethon.tl.types import User, Chat, Channel
-from telethon.errors import ChatAdminRequiredError
+from telethon.tl.types import Channel
 import os
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--urls', nargs='+', type=str)
+parser.add_argument("--urls", nargs="+", type=str)
 args = parser.parse_args()
 
 # Разбираем командную строку
 args = parser.parse_args()
 
 api_id = 21545783
-api_hash = '389839339699f6a919ac6ead583df8fa'
-session_name = 'session.session'
-queryKey = [  'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м',
-  'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ',
-  'ы', 'ь', 'э', 'ю', 'я', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+api_hash = "389839339699f6a919ac6ead583df8fa"
+session_name = "session.session"
+queryKey = [
+    "а",
+    "б",
+    "в",
+    "г",
+    "д",
+    "е",
+    "ё",
+    "ж",
+    "з",
+    "и",
+    "й",
+    "к",
+    "л",
+    "м",
+    "н",
+    "о",
+    "п",
+    "р",
+    "с",
+    "т",
+    "у",
+    "ф",
+    "х",
+    "ц",
+    "ч",
+    "ш",
+    "щ",
+    "ъ",
+    "ы",
+    "ь",
+    "э",
+    "ю",
+    "я",
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+]
 
-logging.basicConfig(level=logging.INFO, format='[%(asctime)s] [%(levelname)s] %(message)s')
+logging.basicConfig(
+    level=logging.INFO, format="[%(asctime)s] [%(levelname)s] %(message)s"
+)
 logger = logging.getLogger(__name__)
 
-file_handler = logging.FileHandler('chat_parser.log')
+file_handler = logging.FileHandler("chat_parser.log")
 file_handler.setLevel(logging.INFO)
-file_handler.setFormatter(logging.Formatter('[%(asctime)s] [%(levelname)s] %(message)s'))
+file_handler.setFormatter(
+    logging.Formatter("[%(asctime)s] [%(levelname)s] %(message)s")
+)
 
 # Добавление обработчика к логгеру
 logger.addHandler(file_handler)
 
+
 def get_username(entity):
-    if hasattr(entity, 'username') and entity.username is not None:
+    if hasattr(entity, "username") and entity.username is not None:
         return entity.username
     else:
         return None
+
 
 def serialize_participant(participant):
     return {
         "user_id": participant.id,
         "first_name": participant.first_name,
-        "last_name": participant.last_name if hasattr(participant, 'last_name') else None,
-        "last_online": participant.status.was_online.strftime('%Y-%m-%d %H:%M:%S')
-        if participant.status and hasattr(participant.status, 'was_online') else None,
+        "last_name": participant.last_name
+        if hasattr(participant, "last_name")
+        else None,
+        "last_online": participant.status.was_online.strftime("%Y-%m-%d %H:%M:%S")
+        if participant.status and hasattr(participant.status, "was_online")
+        else None,
         "scam": participant.scam,
         "fake": participant.fake,
         "premium": participant.premium,
@@ -55,43 +131,55 @@ def serialize_participant(participant):
         "phone": participant.phone,
         "bot": participant.bot,
         "verified": participant.verified,
-        "image": hasattr(participant, 'photo') and participant.photo is not None
+        "image": hasattr(participant, "photo") and participant.photo is not None,
     }
 
+
 def save_user_data(user_data, file_path):
-    with open(file_path, 'w') as file:
+    with open(file_path, "w") as file:
         json.dump(user_data, file, ensure_ascii=False, indent=4)
     logger.info("Данные успешно сохранены в файл.")
-    logger.info("-------------------------------------------------------------------------------")
+    logger.info(
+        "-------------------------------------------------------------------------------"
+    )
 
 
 async def main(chat_urls_or_usernames, file_path):
     user_data = {"chats": {}, "accounts": {}}
     try:
+        print()
         async with TelegramClient(session_name, api_id, api_hash) as client:
             print(client)
             for chat_url_or_username in chat_urls_or_usernames:
                 try:
                     chat = await client.get_entity(chat_url_or_username)
                     if not chat.megagroup:
-                        logger.error(f"Чат {chat_url_or_username} не распаршен, он не является мегагруппой")
+                        logger.error(
+                            f"Чат {chat_url_or_username} не распаршен, он не является мегагруппой"
+                        )
                         continue
                 except Exception as e:
-                    logger.error(f"Чат {chat_url_or_username} не распаршен, произошла ошибка. {e}")
+                    logger.error(
+                        f"Чат {chat_url_or_username} не распаршен, произошла ошибка. {e}"
+                    )
                     continue
 
                 logger.info(f"Обработка чата: {chat.title}")
                 chat_data = {
                     "chat_id": chat.id,
-                    "title": chat.title if hasattr(chat, 'title') else None,
-                    "last_online": chat.date.strftime('%Y-%m-%d %H:%M:%S') if chat.date and hasattr(chat, 'date') else None
+                    "title": chat.title if hasattr(chat, "title") else None,
+                    "last_online": chat.date.strftime("%Y-%m-%d %H:%M:%S")
+                    if chat.date and hasattr(chat, "date")
+                    else None,
                 }
                 user_data["chats"][chat_url_or_username] = chat_data
 
                 try:
                     total_messages = (await client.get_messages(chat, 1)).total
                 except Exception as e:
-                    logger.error(f"Произошла ошибка при получении сообщений в чате: {chat.title}, {e}")
+                    logger.error(
+                        f"Произошла ошибка при получении сообщений в чате: {chat.title}, {e}"
+                    )
                     continue
 
                 processed_participants = 0
@@ -103,66 +191,105 @@ async def main(chat_urls_or_usernames, file_path):
 
                     for participant in participants:
                         processed_participants += 1
-                        logger.info(f"[{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] Обработка участника {processed_participants}/{total_participants}")
+                        logger.info(
+                            f"[{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] Обработка участника {processed_participants}/{total_participants}"
+                        )
 
-                        if not isinstance(participant, Channel) and not getattr(participant, 'bot', False):
+                        if not isinstance(participant, Channel) and not getattr(
+                            participant, "bot", False
+                        ):
                             username = get_username(participant)
                             if username is not None:
                                 if username not in user_data["accounts"]:
                                     user_data["accounts"][username] = {
                                         "user_id": participant.id,
                                         "first_name": participant.first_name,
-                                        "last_name": participant.last_name if hasattr(participant, 'last_name') else None,
-                                        "last_online": participant.status.was_online.strftime('%Y-%m-%d %H:%M:%S')
-                                        if participant.status and hasattr(participant.status, 'was_online') else None,
-                                        "date_updated": datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
-                                        "chats": {
-                                            chat_url_or_username: []
-                                        }
+                                        "last_name": participant.last_name
+                                        if hasattr(participant, "last_name")
+                                        else None,
+                                        "last_online": participant.status.was_online.strftime(
+                                            "%Y-%m-%d %H:%M:%S"
+                                        )
+                                        if participant.status
+                                        and hasattr(participant.status, "was_online")
+                                        else None,
+                                        "date_updated": datetime.datetime.now().strftime(
+                                            "%Y-%m-%d %H:%M:%S"
+                                        ),
+                                        "chats": {chat_url_or_username: []},
                                     }
                                 else:
-                                    if chat_url_or_username not in user_data["accounts"][username]["chats"]:
-                                        user_data["accounts"][username]["chats"][chat_url_or_username] = []
+                                    if (
+                                        chat_url_or_username
+                                        not in user_data["accounts"][username]["chats"]
+                                    ):
+                                        user_data["accounts"][username]["chats"][
+                                            chat_url_or_username
+                                        ] = []
 
                                 full_user_info = serialize_participant(participant)
-                                user_data["accounts"][username]["full_user_info"] = full_user_info
+                                user_data["accounts"][username][
+                                    "full_user_info"
+                                ] = full_user_info
 
                 processed_messages = 0
 
                 async for message in client.iter_messages(chat, limit=10000):
                     sender = message.sender
-                    if sender is not None and not isinstance(sender, Channel) and not getattr(sender, 'bot', False):
+                    if (
+                        sender is not None
+                        and not isinstance(sender, Channel)
+                        and not getattr(sender, "bot", False)
+                    ):
                         username = get_username(sender)
                         if username is not None:
                             if username not in user_data["accounts"]:
                                 user_data["accounts"][username] = {
                                     "user_id": sender.id,
                                     "first_name": sender.first_name,
-                                    "last_name": sender.last_name if hasattr(sender, 'last_name') else None,
-                                    "last_online": sender.status.was_online.strftime('%Y-%m-%d %H:%M:%S')
-                                    if sender.status and hasattr(sender.status, 'was_online') else None,
-                                    "date_updated": datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
-                                    "chats": {
-                                        chat_url_or_username: []
-                                    }
+                                    "last_name": sender.last_name
+                                    if hasattr(sender, "last_name")
+                                    else None,
+                                    "last_online": sender.status.was_online.strftime(
+                                        "%Y-%m-%d %H:%M:%S"
+                                    )
+                                    if sender.status
+                                    and hasattr(sender.status, "was_online")
+                                    else None,
+                                    "date_updated": datetime.datetime.now().strftime(
+                                        "%Y-%m-%d %H:%M:%S"
+                                    ),
+                                    "chats": {chat_url_or_username: []},
                                 }
                             else:
-                                if chat_url_or_username not in user_data["accounts"][username]["chats"]:
-                                    user_data["accounts"][username]["chats"][chat_url_or_username] = []
+                                if (
+                                    chat_url_or_username
+                                    not in user_data["accounts"][username]["chats"]
+                                ):
+                                    user_data["accounts"][username]["chats"][
+                                        chat_url_or_username
+                                    ] = []
 
                             full_user_info = serialize_participant(sender)
-                            user_data["accounts"][username]["full_user_info"] = full_user_info
+                            user_data["accounts"][username][
+                                "full_user_info"
+                            ] = full_user_info
 
                             processed_messages += 1
                             progress = processed_messages / total_messages * 100
-                            logger.info(f"[{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] Обработка сообщений: {processed_messages}/{total_messages} ({progress:.2f}%)")
+                            logger.info(
+                                f"[{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] Обработка сообщений: {processed_messages}/{total_messages} ({progress:.2f}%)"
+                            )
 
                             if message.text and message.text.strip() != "":
-                                user_data["accounts"][username]["chats"][chat_url_or_username].append(message.text)
+                                user_data["accounts"][username]["chats"][
+                                    chat_url_or_username
+                                ].append(message.text)
     except Exception as e:
         logger.error(f"Произошла глобальная ошибка. {e}")
 
     save_user_data(user_data, file_path)
+
 
 json_folder = "result"
 os.makedirs(json_folder, exist_ok=True)
